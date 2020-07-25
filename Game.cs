1d6ad39ebf6b039b6be415bcf24114c09fe9f82f
mgrauzina <55634975+mgrauzina@users.noreply.github.com>
2020-07-25 16:05:19 +0300

    Add files via upload

--- Game.cs
+++ Game.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Hangman
 {
     class Game
     {
         List<string> wordList = new List<string>();
+        char[] word;
+        char[] result;
 
         //Game starts
         public void PlayGame()
         {
             Greeting();
             AddWords();
-            GetWord();
             GuessWord();
         }
-        //Greeting the player
+
+        //Greeting the player, explaining rules        
         private void Greeting()
         {
-            Console.WriteLine("WELCOME TO THE HANGMAN GAME!");
-            Console.Write("What's your name? "); var userName = Console.ReadLine();
-            Console.WriteLine($"Great! Let's begin, {userName}!");
-            Console.WriteLine();
+            Console.WriteLine("WELCOME TO THE HANGMAN GAME!"); Console.WriteLine(); 
+            Console.WriteLine("GAME DESCRIPTION:"); 
+            Console.WriteLine("1. The computer chooses a word from a secret list."); 
+            Console.WriteLine("2. You have to guess the word until your lives run out."); Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(); Console.SetCursorPosition(0, 7);
+            Console.ForegroundColor = ConsoleColor.Green;            
+            Console.WriteLine("Good luck!"); Thread.Sleep(1000); Console.ResetColor();
         }
+
         //Adding words to the list
         private void AddWords()
         {
@@ -40,30 +49,56 @@ namespace Hangman
         {
             Random random = new Random();
             int randomWordNumber = random.Next(1, wordList.Count);
-            Console.WriteLine($"This word contains {wordList[randomWordNumber].Length} letters.");
-            for (int i = 0; i <= wordList[randomWordNumber].Length; i++)
+
+            word = wordList[randomWordNumber].ToCharArray();
+            result = new char[wordList[randomWordNumber].Length];
+
+            for (int i = 0; i < word.Length; i++)
             {
-                Console.Write("_");
+                result[i] = '_';
             };
+
+            Console.Clear();
+            Console.WriteLine("LET'S PLAY THE HANGMAN GAME!");
             Console.WriteLine();
+            Console.WriteLine(result);
         }
 
-        //Method for word guessing       
-        //Currently the loop keeps going until you enter a single letter.
-        //Need to add another method for when the user input is correct. 
-        //After that there are two options: whether the letter is or is not a part of the word.
+        //Method for word guessing      
         private void GuessWord()
         {
-            Console.WriteLine();
-            Console.Write("Guess a letter: "); string userLetter = Console.ReadLine();
-
-            if (!ValidateInput(userLetter))
+            GetWord();
+            bool _gameNoStop = true;
+            while (_gameNoStop)
             {
-                while (!ValidateInput(userLetter))
+                Console.WriteLine();
+                Console.Write("Guess a letter: ");
+
+                string userLetter = Console.ReadLine();
+
+                if (!ValidateInput(userLetter))
                 {
-                    Console.Write("You have to guess by one letter! Guess again: ");
-                    userLetter = Console.ReadLine();
+                    while (!ValidateInput(userLetter))
+                    {
+                        Console.Write("You have to guess by one letter! Guess again: ");
+                        userLetter = Console.ReadLine();
+                    }
                 }
+                var letter = userLetter.ToCharArray();
+                updateWord(letter[0]);
+
+                Console.Clear();
+                Console.WriteLine("LET'S PLAY THE HANGMAN GAME!");
+                Console.WriteLine();
+                Console.WriteLine(result);
+
+                _gameNoStop = result.Any(m => m == '_');             
+             }
+            if (!_gameNoStop)
+            {
+                Console.WriteLine(); Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("You WON!"); Console.ResetColor();
+                ContinueGame();
             }
         }
 
@@ -86,6 +121,44 @@ namespace Hangman
                 return false;
             }
             return true;
+        }
+
+        //Updating the word after each guess
+        private void updateWord(char letter)
+        {
+            for (int i = 0; i <= word.Length - 1; i++)
+            {
+                if (word[i] == letter)
+                {
+                    result[i] = word[i];
+                }
+            }
+        }
+
+        //Asking the user if he/she wants to continue playing the game. Input has to be 'y' or 'n'.
+        private void ContinueGame()
+        {
+            Console.WriteLine(); 
+            Console.Write("Continue game? (y/n): "); 
+
+            var userAnswer = Console.ReadLine();
+
+            while (userAnswer != "y" && userAnswer != "n")
+            {            
+            Console.Write("Continue game? (y/n): "); 
+            userAnswer = Console.ReadLine();
+            }
+
+            if (userAnswer == "n")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Thanks for playing the Hangman game!");
+            }
+
+            if (userAnswer == "y")
+            {
+                GuessWord();
+            }
         }
     }
 }
